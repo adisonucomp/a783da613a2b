85bfe4b40926f0fa52ac6dac114c06596f2ae4a9
Adison Jimenez <adisonzenemij@gmail.com>
2026-08-21 13:00:48 -0500

    feat: e2f4de1c-aa8c-43c3-9c1d-7bab64804193

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -15,10 +15,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the application router outlet', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, a783da613a2b');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
