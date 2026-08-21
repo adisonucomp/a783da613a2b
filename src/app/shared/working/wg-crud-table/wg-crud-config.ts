54b04672e323ceb48da08681783c43bf03b68081
Adison Jimenez <adisonzenemij@gmail.com>
2026-08-21 15:37:50 -0500

    feat: 649b2ad9-7f5f-41c6-a7ec-26dcada6537c

--- src/app/shared/working/wg-crud-table/wg-crud-config.ts
+++ src/app/shared/working/wg-crud-table/wg-crud-config.ts
@@ -56,7 +56,7 @@ export const deviceDataConfig: WgCrudConfig = {
     { key: 'graphicCardId', label: 'Tarjeta Gráfica', relation: 'graphic-card' },
     { key: 'operatingSystemId', label: 'Sistema Operativo', relation: 'operating-system' },
     { key: 'typeProcessorId', label: 'Tipo de Procesador', relation: 'type-processor' },
-    { key: 'fdDetail', label: 'Detalle', type: 'textarea', fullWidth: true },
+    { key: 'fdDetail', label: 'Detalle', type: 'textarea', fullWidth: true, required: false },
     { key: 'fdImage', label: 'Imagen', type: 'image', fullWidth: true },
   ],
 };
