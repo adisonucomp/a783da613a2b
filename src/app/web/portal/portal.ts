6b3b5fc0fba8f06832b297bce3c8c64536fd00d0
Adison Jimenez <adisonzenemij@gmail.com>
2026-08-21 12:00:34 -0500

    feat: 85c688b0-9f66-4960-bef4-93f9eb1f50c9

--- src/app/web/portal/portal.ts
+++ src/app/web/portal/portal.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PtNavbar } from '../../shared/portal/pt-navbar/pt-navbar';
+import { PtFooter } from '../../shared/portal/pt-footer/pt-footer';
 
 @Component({
-  imports: [PtNavbar, RouterOutlet],
+  imports: [PtNavbar, RouterOutlet, PtFooter],
   selector: 'app-portal',
   styleUrl: './portal.css',
   templateUrl: './portal.html',
