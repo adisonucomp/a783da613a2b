752905f6827dd306e9152b6f593c848867660fd9
Adison Jimenez <adisonzenemij@gmail.com>
2026-08-21 19:56:30 -0500

    feat: fd3e48f1-686f-4c57-b1aa-30107bc77900

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { PtHome } from './web/portal/pt-home/pt-home';
 import { PtRegister } from './web/portal/pt-register/pt-register';
 import { PtA6ac2e09 } from './web/portal/pt-a6ac2e09/pt-a6ac2e09';
 import { PtB4c4c7b1 } from './web/portal/pt-b4c4c7b1/pt-b4c4c7b1';
-import { PtB8043c54 } from './web/portal/pt-b8043c54/pt-b8043c54';
 import { PtC98391c6 } from './web/portal/pt-c98391c6/pt-c98391c6';
 import { PtD0112a5a } from './web/portal/pt-d0112a5a/pt-d0112a5a';
 import { PtProduct } from './web/portal/pt-product/pt-product';
@@ -35,11 +34,10 @@ export const routes: Routes = [
       { path: '', pathMatch: 'full', redirectTo: 'home' },
       { path: 'home', component: PtHome }, // Componente Inicio
       { path: 'register', component: PtRegister, canActivate: [guestGuard] }, // Componente Registro
-      { path: 'a6ac2e09', component: PtA6ac2e09 }, // Componente graphic_card - Tarjetas Gráficas
-      { path: 'b4c4c7b1', component: PtB4c4c7b1 }, // Componente brand_device - Marcas de Dispositivos
-      { path: 'b8043c54', component: PtB8043c54 }, // Componente device_data - Dispositivos
-      { path: 'c98391c6', component: PtC98391c6 }, // Componente brand_processor - Marcas de Procesadores
-      { path: 'd0112a5a', component: PtD0112a5a }, // Componente operating_system - Sistemas Operativos
+      { path: 'graphic-card', component: PtA6ac2e09 }, // Componente graphic_card - Tarjetas Gráficas
+      { path: 'brand-device', component: PtB4c4c7b1 }, // Componente brand_device - Marcas de Dispositivos
+      { path: 'brand-processor', component: PtC98391c6 }, // Componente brand_processor - Marcas de Procesadores
+      { path: 'operating-system', component: PtD0112a5a }, // Componente operating_system - Sistemas Operativos
       { path: 'product/:id', component: PtProduct }, // Componente detalle de producto
     ],
   },
