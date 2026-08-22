7ca7328a2733059463e7ae40d898aadb03a24266
Adison Jimenez <adisonzenemij@gmail.com>
2026-08-21 20:43:45 -0500

    feat: 5da091c1-8f18-4fe7-926c-0e5d1c5ccb2a

--- src/app/shared/portal/pt-navbar/pt-navbar.ts
+++ src/app/shared/portal/pt-navbar/pt-navbar.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import Swal from 'sweetalert2';
 import { AuthSession } from '../../../services/core/auth-session/auth-session';
 import { SessionTimer } from '../../../services/core/session-timer/session-timer';
 import { ThemeToggle } from '../../theme-toggle/theme-toggle';
 
 @Component({
-  imports: [RouterLink, ThemeToggle],
+  imports: [RouterLink, RouterLinkActive, ThemeToggle],
   selector: 'app-pt-navbar',
   styleUrl: './pt-navbar.css',
   templateUrl: './pt-navbar.html',
